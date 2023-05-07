d47f6524a4145d2b08997a1e463f46954ac4249c
김시후 <08sihu@gmail.com>
2023-05-07 15:50:16 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo Welcome to Shuffle!
+echo Installation will start automatically after a while.
+
+cd ~
+mkdir .shuffle
+cd .shuffle
+wget -O shuffle.zip https://github.com/shflterm/shuffle/releases/download/release/v1.0/shuffle.zip
+unzip shuffle.zip
+
+echo shuffle has been successfully installed in '~/.shuffle/bin/shuffle'.
