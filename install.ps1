0bdbc3e0b9827279f3f5aff6b6477c1d3b6df140
김시후 <08sihu@gmail.com>
2023-05-21 22:37:14 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,14 +1,21 @@
-echo "Welcome to Shuffle!"
+echo "\t  /$$$$$$        /$$   /$$       /$$$$$$$$       /$$      \n
+\t /$$__  $$      | $$  | $$      | $$_____/      | $$      \n
+\t| $$  \\__/      | $$  | $$      | $$            | $$      \n
+\t|  $$$$$$       | $$$$$$$$      | $$$$$         | $$      \n
+\t \\____  $$      | $$__  $$      | $$__/         | $$      \n
+\t /$$  \\ $$      | $$  | $$      | $$            | $$      \n
+\t|  $$$$$$/      | $$  | $$      | $$            | $$$$$$$$\n
+\t \\______/       |__/  |__/      |__/            |________/\n"
 echo "Installation will start automatically after a while."
 
-Set-Location $env:APPDATA
-New-Item .shuffle -ItemType Directory
-Set-Location .shuffle
+Set-Location $env:APPDATA | out-null
+New-Item .shuffle -ItemType Directory | out-null
+Set-Location .shuffle | out-null
 
-Invoke-WebRequest https://github.com/shflterm/install/raw/main/bin.zip -O bin.zip
-Expand-Archive bin.zip -DestinationPath .
+Invoke-WebRequest https://github.com/shflterm/install/raw/main/bin.zip -O bin.zip | out-null
+Expand-Archive bin.zip -DestinationPath . | out-null
 
 echo "Install the default app."
-%APPDATA%/.shuffle/bin/shuffle shfl apps add cd dir help list mk rm
+%APPDATA%/.shuffle/bin/shuffle shfl apps add cd dir help list mk rm | out-null
 
 echo "shuffle has been successfully installed in '%APPDATA%/.shuffle/bin/shuffle'."
